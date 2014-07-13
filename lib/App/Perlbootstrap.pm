f0d72103ec28d3b299315495538c75f5f15f74e8
mudler <mudler@dark-lab.net>
2014-07-13 09:42:25 +0200

    first commit

---
+++ lib/App/Perlbootstrap.pm
@@ -0,0 +1,39 @@
+package App::Perlbootstrap;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Perlbootstrap - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use App::Perlbootstrap;
+
+=head1 DESCRIPTION
+
+App::Perlbootstrap is
+
+=head1 AUTHOR
+
+mudler E<lt>mudler@dark-lab.netE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- mudler
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
