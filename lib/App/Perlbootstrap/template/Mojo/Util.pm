0270be5ead4262a72cf7401399f9626859f7d825
mudler <mudler@dark-lab.net>
2014-08-16 11:25:47 +0200

    added terminal color, utf8 symbols, updated POD

--- lib/App/Perlbootstrap/template/Mojo/Util.pm
+++ lib/App/Perlbootstrap/template/Mojo/Util.pm
@@ -1,25 +1,69 @@
 package App::Perlbootstrap::template::Mojo::Util;
 use base 'Exporter';
 use strict;
+use utf8;
+use Encode;
+use Term::ANSIColor;
 use warnings;
 use MIME::Base64 qw(decode_base64 encode_base64);
-our @EXPORT = ();
-our @EXPORT_OK = ( qw(b64_decode b64_encode class_to_path info error warning), @EXPORT );
+our @EXPORT    = (qw(info error warning));
+our @EXPORT_OK = (
+    qw(b64_decode b64_encode class_to_path  dialog_yes_default dialog_no_default),
+    @EXPORT
+);
 
 sub info {
-    print "[info] - @_  \n";
+    my @msg = @_;
+    print color 'bold green';
+    print encode_utf8('╠ ');
+    print color 'bold white';
+    print join( "\n", @msg ), "\n";
+    print color 'reset';
 }
 
 sub error {
-    print STDERR "[error] - @_  \n";
+    my @msg = @_;
+    print STDERR color 'bold red';
+    print STDERR encode_utf8('☢☢☢ ☛  ');
+    print STDERR color 'bold white';
+    print STDERR join( "\n", @msg ), "\n";
+    print STDERR color 'reset';
 }
 
 sub warning {
-    print "[warning] - @_  \n";
+    my @msg = @_;
+    print color 'bold yellow';
+    print encode_utf8('☛ ');
+    print color 'bold white';
+    print join( "\n", @msg ), "\n";
+    print color 'reset';
 }
-sub b64_decode { decode_base64($_[0]) }
-sub b64_encode { encode_base64($_[0], $_[1]) }
-sub class_to_path { join '.', join('/', split /::|'/, shift), 'pm' }
+
+sub dialog_yes_default {
+    my $msg = shift;
+    local $|;
+    &warning("$msg (Y/n)");
+    my $a = <STDIN>;
+    chomp $a;
+    return 0 if ( $a =~ /n/ );
+    return 1 if $a =~ /y/;
+    return 1;    # default to Y
+}
+
+sub dialog_no_default {
+    my $msg = shift;
+    local $|;
+    &warning("$msg (y/N)");
+    my $a = <STDIN>;
+    chomp $a;
+    return 0 if ( $a =~ /n/ );
+    return 1 if $a =~ /y/;
+    return 0;    # default to Y
+}
+
+sub b64_decode    { decode_base64( $_[0] ) }
+sub b64_encode    { encode_base64( $_[0], $_[1] ) }
+sub class_to_path { join '.', join( '/', split /::|'/, shift ), 'pm' }
 1;
 __END__
 =encoding utf8
@@ -37,7 +81,7 @@ App::Perlbootstrap::template::Mojo::Util - Minimal base Exporter module for App:
 
 =head1 DESCRIPTION
 
-L<App::Perlbootstrap::template::Mojo::Util> is a simple Exporter class for L<App::Perlbootstrap::template::Mojo>,  it exports by default, just for sake of example convenients functions for printing to console.
+L<App::Perlbootstrap::template::Mojo::Util> is a simple Exporter class for L<App::Perlbootstrap::template::Mojo>,  it exports by default, just for sake of example convenients functions for printing to console. The purpose of
 
 =head1 FUNCTIONS
 
@@ -59,8 +103,32 @@ print the output to STDERR
 
 print the output at the console
 
+=head2 dialog_yes_default
+
+    dialog_yes_default "Something"
+
+Asks the user a question, defaults to y
+
+=head2 dialog_no_default
+
+    dialog_no_default "Something"
+
+Asks the user a question, defaults to n
+
+=head2 b64_decode / b64_encode
+
+    b64_decode "Something"
+
+decode/encode in base 64
+
+=head2 class_to_path
+
+    class_to_path "some::module"
+
+it returns the path (some/module.pm)
+
 =head1 SEE ALSO
 
-L<App::Perlbootstrap::template::Mojo>, L<Exporter>.
+L<App::Perlbootstrap::template::Mojo>, L<Exporter> , L<Mojo::Utils>.
 
 =cut
